50ebfb441379411c46399300de712a14d8c05133
Yash <yashgogia2001@gmail.com>
2022-09-13 18:48:41 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const words = [
+  "Freelancing",
+  "Web Development",
+  "Front-end Development",
+  "Software Development",
+];
+let i = 0;
+let timer;
+
+function typingEffect() {
+  let word = words[i].split("");
+  var loopTyping = function () {
+    if (word.length > 0) {
+      document.getElementById("word").innerHTML += word.shift();
+    } else {
+      deletingEffect();
+      return false;
+    }
+    timer = setTimeout(loopTyping, 200);
+  };
+  loopTyping();
+}
+
+function deletingEffect() {
+  let word = words[i].split("");
+  var loopDeleting = function () {
+    if (word.length > 0) {
+      word.pop();
+      document.getElementById("word").innerHTML = word.join("");
+    } else {
+      if (words.length > i + 1) {
+        i++;
+      } else {
+        i = 0;
+      }
+      typingEffect();
+      return false;
+    }
+    timer = setTimeout(loopDeleting, 200);
+  };
+  loopDeleting();
+}
+
+typingEffect();
+
+$(".js-scroll-trigger").click(function () {
+  $(".navbar-collapse").collapse("hide");
+});
+$(document).ready(function () {
+  $(".toast").toast("show");
+});
